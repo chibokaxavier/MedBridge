af235c7656953a88ac7147b0130a45258430ba8e
manlikexavy <chibokaxavier@gmail.com>
2024-07-26 10:40:35 +0100

    added the images and gifs need , done installing and setting up shadecn and also our custom styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import {cn} '@'
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -22,7 +23,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn}>{children}</body>
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
+import Patient from "@/components/Form/PatientForm";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      
-    </main>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container gap-2 max-w-[496px]">
+          <Image
+            src="/logo.png"
+            height={1000}
+            width={2000}
+            alt="logo"
+            className="mb-12 h-10 w-fit"
+          />
+          <Patient />
+          <div className="text-14-regular flex justify-between mt-20">
+            <p className="justify-items-end xl:text-left text-dark-600">
+              © 2024 MedBridge
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
