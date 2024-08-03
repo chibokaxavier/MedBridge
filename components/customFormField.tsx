c03a0bc74a558346ff664f092739af7cc39e9268
manlikexavy <chibokaxavier@gmail.com>
2024-08-03 14:34:04 +0100

    allergirs input completed

--- components/customFormField.tsx
+++ components/customFormField.tsx
@@ -16,6 +16,7 @@ import { FaCalendarAlt } from "react-icons/fa";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select";
+import { Textarea } from "./ui/textarea";
 
 interface CustomProps {
   control: any;
@@ -58,13 +59,18 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
           </FormControl>
         </div>
       );
-      
-case FormFieldType.TEXTAREA:
-  return (
-    <Formcontrol>
-      
-    </Formcontrol>
-  )
+
+    case FormFieldType.TEXTAREA:
+      return (
+        <FormControl>
+          <Textarea
+            placeholder={placeholder}
+            {...field}
+            className="shad-textArea"
+            disabled={props.disabled}
+          />
+        </FormControl>
+      );
     case FormFieldType.PHONE_INPUT:
       return (
         <FormControl>
