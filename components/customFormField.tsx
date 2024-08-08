6ebb196cb4c2fcb6bca259a4cb45c18fdbe35c08
manlikexavy <chibokaxavier@gmail.com>
2024-08-08 10:44:32 +0100

    consent and privacy section complete

--- components/customFormField.tsx
+++ components/customFormField.tsx
@@ -16,6 +16,7 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Select, SelectContent, SelectTrigger, SelectValue } from "./ui/select";
 import { Textarea } from "./ui/textarea";
+import { Checkbox } from "./ui/checkbox";
 
 interface CustomProps {
   control: any;
@@ -116,11 +117,20 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
         </FormControl>
       );
     case FormFieldType.CHECKBOX:
-      <FormControl>
-        <div className="flex items-center gap-4">
-          
-        </div>
-      </FormControl>;
+      return (
+        <FormControl>
+          <div className="flex items-center gap-4">
+            <Checkbox
+              id={props.name}
+              checked={field.value}
+              onCheckedChange={field.onChange}
+            />
+            <label htmlFor={props.name} className="checkbox-label">
+              {props.label}
+            </label>
+          </div>
+        </FormControl>
+      );
     case FormFieldType.SKELETON:
       return renderSkeleton ? renderSkeleton(field) : null;
 
